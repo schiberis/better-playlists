7a5f1b2a0e5a3786fe85b85f2b0407c93c7a2597
Simas <schiberis@gmail.com>
2018-07-16 22:09:29 +0200

    Initial commit
    Description1

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+let defaulTextColor = '#000';
+let defaultStyle = {
+  color: defaulTextColor
+}
+class Aggregate extends Component {
+  render () {
+    return (
+      <div style={{...defaultStyle, width: '40%', display: 'inline-block'}}>
+        <h2>Number Text</h2>
+      </div>
+    );
+  }
+}
+
+class Filter extends Component {
+  render() {
+    return (
+      <div style={defaultStyle}>
+        <img/>
+        <input type="text"/>
+      </div>
+    );
+  }
+}
+
+class Playlist extends Component {
+  render() {
+    return (
+      <div style={{...defaultStyle, width: '25%', display: 'inline-block'}}>
+        <img/>
+        <h3>Playlist Name</h3>
+        <ul><li>Song 1</li><li>Song 2</li><li>Song 3</li></ul>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <h1>Title world</h1>
+      <Aggregate />
+      <Aggregate />
+      <Filter />
+      <Playlist />
+      <Playlist />
+      <Playlist />
+      <Playlist />
+       </div>
+    );
+  }
+}
+
+export default App;
